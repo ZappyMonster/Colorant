2821d11acb695fddd45e60c4a7ed6c1774dc3b6f
Hafez <82477000+hafyzwithawhy@users.noreply.github.com>
2023-04-17 21:36:59 +0300

    Create main.py
    Updated main.py to v1.1

--- main.py
+++ main.py
@@ -1,64 +1,59 @@
 import os
 import time
 import keyboard
+import pyautogui
 from termcolor import colored
 from colorant import Colorant
 
-os.system('color')
-os.system('title Colorant')
+#Settings
+TOGGLE_KEY = 'F1'  # Toggle on/off colorant key
+XFOV = 50  # X-Axis FOV
+YFOV = 50  # Y-Axis FOV
+INGAME_SENSITIVITY = 0.4 # Replace this with the your in-game sensitivity value
+FLICKSPEED = 1.07437623 * (INGAME_SENSITIVITY ** -0.9936827126)  # Calculate flick speed
+MOVESPEED = 1 / (5 * INGAME_SENSITIVITY) # Calculate move speed
 
-TOGGLE_KEY = 'F1'
-FOV = 50
-CENTER_X, CENTER_Y = 1920 // 2, 1080 // 2
-
-class Main:
-    def __init__(self):
-        self.colorant = Colorant(CENTER_X - FOV // 2, CENTER_Y - FOV // 2, FOV)
-        self.status = 'Disabled'
-
-    def print(self):
-        print(colored('''
-                         ▄████▄   ▒█████   ██▓     ▒█████   ██▀███   ▄▄▄       ███▄    █ ▄▄▄█████▓
-                        ▒██▀ ▀█  ▒██▒  ██▒▓██▒    ▒██▒  ██▒▓██ ▒ ██▒▒████▄     ██ ▀█   █ ▓  ██▒ ▓▒
-                        ▒▓█    ▄ ▒██░  ██▒▒██░    ▒██░  ██▒▓██ ░▄█ ▒▒██  ▀█▄  ▓██  ▀█ ██▒▒ ▓██░ ▒░
-                        ▒▓▓▄ ▄██▒▒██   ██░▒██░    ▒██   ██░▒██▀▀█▄  ░██▄▄▄▄██ ▓██▒  ▐▌██▒░ ▓██▓ ░ 
-                        ▒ ▓███▀ ░░ ████▓▒░░██████▒░ ████▓▒░░██▓ ▒██▒ ▓█   ▓██▒▒██░   ▓██░  ▒██▒ ░ 
-                        ░ ░▒ ▒  ░░ ▒░▒░▒░ ░ ▒░▓  ░░ ▒░▒░▒░ ░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ▒░   ▒ ▒   ▒ ░░   
-                          ░  ▒     ░ ▒ ▒░ ░ ░ ▒  ░  ░ ▒ ▒░   ░▒ ░ ▒░  ▒   ▒▒ ░░ ░░   ░ ▒░    ░    
-                        ░        ░ ░ ░ ▒    ░ ░   ░ ░ ░ ▒    ░░   ░   ░   ▒      ░   ░ ░   ░      
-                        ░ ░          ░ ░      ░  ░    ░ ░     ░           ░  ░         ░          
-                        ░                                                                         
-                                                  COLOR AIMBOT - v1.0''', 'magenta'))
-        print()
-        print(colored('[Info]', 'green'), colored('Set enemies to', 'white'), colored('Purple', 'magenta'))
-        print(colored('[Info]', 'green'), colored(f'Press {TOGGLE_KEY} to toggle ON/OFF', 'white'))
-        print(colored('[Info]', 'green'), colored('Default settings are', 'white'),
-              colored('RightMB', 'magenta'), colored('= Aimbot,', 'white'),
-              colored('LeftAlt', 'magenta'), colored('= Triggerbot', 'white'))
-        print(colored('[Info]', 'green'), colored('GitHub Repo:', 'white'),
-              '\033[35;4mhttps://github.com/hafyzwithawhy/Colorant\033[0m')
-        print(colored('[Info]', 'green'), colored('Made By', 'white'), colored('Hafez#6866', 'magenta'))
-
-    def toggle_aimbot(self):
-        if keyboard.is_pressed(TOGGLE_KEY):
-            self.colorant.toggle()
-            self.status = 'Enabled ' if self.colorant.toggled else 'Disabled'
-
-    def run(self):
-        try:
-            self.print()
-            while True:
-                self.toggle_aimbot()
-                print(f'\r{colored("[Status]", "green")} {colored(self.status, "white")}', end='')
-                time.sleep(0.01)
-        except (KeyboardInterrupt, SystemExit):
-            print(colored('\n[Info]', 'green'), colored('Exiting...', 'white') + '\n')
-        finally:
-            self.colorant.close()
+monitor = pyautogui.size()
+CENTER_X, CENTER_Y = monitor.width // 2, monitor.height // 2
 
 def main():
-    aimbot = Main()
-    aimbot.run()
+    os.system('title Colorant')
+    colorant = Colorant(CENTER_X - XFOV // 2, CENTER_Y - YFOV // 2, XFOV, YFOV, FLICKSPEED, MOVESPEED)
+    print(colored('''
+                     ▄████▄   ▒█████   ██▓     ▒█████   ██▀███   ▄▄▄       ███▄    █ ▄▄▄█████▓
+                    ▒██▀ ▀█  ▒██▒  ██▒▓██▒    ▒██▒  ██▒▓██ ▒ ██▒▒████▄     ██ ▀█   █ ▓  ██▒ ▓▒
+                    ▒▓█    ▄ ▒██░  ██▒▒██░    ▒██░  ██▒▓██ ░▄█ ▒▒██  ▀█▄  ▓██  ▀█ ██▒▒ ▓██░ ▒░
+                    ▒▓▓▄ ▄██▒▒██   ██░▒██░    ▒██   ██░▒██▀▀█▄  ░██▄▄▄▄██ ▓██▒  ▐▌██▒░ ▓██▓ ░ 
+                    ▒ ▓███▀ ░░ ████▓▒░░██████▒░ ████▓▒░░██▓ ▒██▒ ▓█   ▓██▒▒██░   ▓██░  ▒██▒ ░ 
+                    ░ ░▒ ▒  ░░ ▒░▒░▒░ ░ ▒░▓  ░░ ▒░▒░▒░ ░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ▒░   ▒ ▒   ▒ ░░   
+                      ░  ▒     ░ ▒ ▒░ ░ ░ ▒  ░  ░ ▒ ▒░   ░▒ ░ ▒░  ▒   ▒▒ ░░ ░░   ░ ▒░    ░    
+                    ░        ░ ░ ░ ▒    ░ ░   ░ ░ ░ ▒    ░░   ░   ░   ▒      ░   ░ ░   ░      
+                    ░ ░          ░ ░      ░  ░    ░ ░     ░           ░  ░         ░          
+                    ░                                                                         
+                                              COLOR AIMBOT - v1.1''', 'magenta'))
+    print()
+    print(colored('[Info]', 'green'), colored('Set enemies to', 'white'), colored('Purple', 'magenta'))
+    print(colored('[Info]', 'green'), colored(f'Press {colored(TOGGLE_KEY, "magenta")} to toggle ON/OFF Colorant', 'white'))
+    print(colored('[Info]', 'green'), colored(f'Press', 'white'), colored('F2', 'magenta'), colored('to toggle ON/OFF Detection Window', 'white'))
+    print(colored('[Info]', 'green'), colored('RightMB', 'magenta'), colored('= Aimbot,', 'white'))
+    print(colored('[Info]', 'green'), colored('LeftAlt', 'magenta'), colored('= Triggerbot', 'white'))
+    print(colored('[Info]', 'green'), colored('LeftCtrl', 'magenta'), colored('= Silentaim', 'white'))
+    print(colored('[Info]', 'green'), colored('GitHub Repo:', 'white'),
+          '\033[35;4mhttps://github.com/hafyzwithawhy/Colorant\033[0m')
+    print(colored('[Info]', 'green'), colored('Made By', 'white'), colored('Hafez#6866', 'magenta'))
+    status = 'Disabled'
+    
+    try:
+        while True:
+            if keyboard.is_pressed(TOGGLE_KEY):
+                colorant.toggle()
+                status = 'Enabled ' if colorant.toggled else 'Disabled'
+            print(f'\r{colored("[Status]", "green")} {colored(status, "white")}', end='')
+            time.sleep(0.01)
+    except (KeyboardInterrupt, SystemExit):
+        print(colored('\n[Info]', 'green'), colored('Exiting...', 'white') + '\n')
+    finally:
+        colorant.close()
 
 if __name__ == '__main__':
     main()
