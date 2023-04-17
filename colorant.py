db141b10512ac8189a574e5b5dcb0077feb2d71a
Hafez <82477000+hafyzwithawhy@users.noreply.github.com>
2023-04-17 21:37:48 +0300

    Update colorant.py
    Updated colorant.py to v1.1

--- colorant.py
+++ colorant.py
@@ -3,38 +3,47 @@ import numpy as np
 import threading
 import time
 import win32api
+import pyautogui
 
 from screen_cap import ScreenCapture
 from mouse import ArduinoMouse
-
+from fov_window import show_detection_window, toggle_window
 
 class Colorant:
     LOWER_COLOR = np.array([140, 110, 150])
     UPPER_COLOR = np.array([150, 195, 255])
 
-    def __init__(self, x, y, grabzone):
+    def __init__(self, x, y, xfov, yfov, FLICKSPEED, MOVESPEED):
         self.arduinomouse = ArduinoMouse()
-        self.grabber = ScreenCapture(x, y, grabzone)
+        self.grabber = ScreenCapture(x, y, xfov, yfov)
+        self.flickspeed = FLICKSPEED
+        self.movespeed = MOVESPEED
         threading.Thread(target=self.listen, daemon=True).start()
         self.toggled = False
-
+        self.window_toggled = False
+        
     def toggle(self):
         self.toggled = not self.toggled
         time.sleep(0.2)
 
     def listen(self):
         while True:
+            if win32api.GetAsyncKeyState(0x71) < 0:
+                toggle_window(self)
+                time.sleep(0.2)
             if win32api.GetAsyncKeyState(0x02) < 0 and self.toggled:
                 self.process("move")
             elif win32api.GetAsyncKeyState(0x12) < 0 and self.toggled:
                 self.process("click")
+            elif win32api.GetAsyncKeyState(0x11) < 0 and self.toggled:
+                self.process("flick")
 
     def process(self, action):
         screen = self.grabber.get_screen()
         hsv = cv2.cvtColor(screen, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, self.LOWER_COLOR, self.UPPER_COLOR)
         dilated = cv2.dilate(mask, None, iterations=5)
-        contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         if not contours:
             return
@@ -42,21 +51,25 @@ class Colorant:
         contour = max(contours, key=cv2.contourArea)
         x, y, w, h = cv2.boundingRect(contour)
         center = (x + w // 2, y + h // 2)
+        y_offset = int(h * 0.3)
 
         if action == "move":
-            cX = x + w // 2
-            cY = y + 9
-            x_diff = cX - self.grabber.grabzone // 2
-            y_diff = cY - self.grabber.grabzone // 2
-            self.arduinomouse.move(x_diff * 0.4, y_diff * 0.4)
-            
-        elif action == "click" and abs(center[0] - self.grabber.grabzone // 2) <= 4 and abs(center[1] - self.grabber.grabzone // 2) <= 10:
+            cX = center[0] + 2
+            cY = y + y_offset
+            x_diff = cX - self.grabber.xfov // 2
+            y_diff = cY - self.grabber.yfov // 2
+            self.arduinomouse.move(x_diff * self.movespeed, y_diff * self.movespeed)
+
+        elif action == "click" and abs(center[0] - self.grabber.xfov // 2) <= 4 and abs(center[1] - self.grabber.yfov // 2) <= 10:
             self.arduinomouse.click()
 
-    def close(self):
-        if hasattr(self, 'arduinomouse'):
-            self.arduinomouse.close()
-        self.toggled = False
-
-    def __del__(self):
-        self.close()
+        elif action == "flick":
+            cX = center[0] + 2
+            cY = y + y_offset
+            x_diff = cX - self.grabber.xfov // 2
+            y_diff = cY - self.grabber.yfov // 2
+            flickx = x_diff * self.flickspeed
+            flicky = y_diff * self.flickspeed
+            self.arduinomouse.flick(flickx, flicky)
+            self.arduinomouse.click()
+            self.arduinomouse.flick(-(flickx), -(flicky))
